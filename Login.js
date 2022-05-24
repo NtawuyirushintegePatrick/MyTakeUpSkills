0ad890531b46a72f7afc5baebcfe24271391d3f2
Ntawuyirushintege Patrick <ntawuyirushintegepatrick@gmail.com>
2022-05-24 08:53:37 +0200

    work on dashboard

--- Login.js
+++ Login.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "admin" && password == "admin123"){
+if ( username == "admin" && password == "admin12"){
 alert ("Login successfully");
 window.location = "dashboard.html"; // Redirecting to other page.
 return false;
